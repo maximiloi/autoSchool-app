167f3d4931beba454e391c8d8382d79dfdb037a2
maximiloi <maximiloi@gmail.com>
2025-01-29 19:08:53 +0300

    refactor: delete unused code

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
-        Привет
-      </h1>
-      <p>рыба рыба</p>
-    </div>
-  );
+  return <div></div>;
 }
