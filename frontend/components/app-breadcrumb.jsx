4f04e5da822df33464b37a40ccaf36c7d7b8d11e
maximiloi <maximiloi@gmail.com>
2025-02-03 10:21:01 +0300

    refactor: add next/navigation

--- frontend/components/app-breadcrumb.jsx
+++ frontend/components/app-breadcrumb.jsx
@@ -4,10 +4,8 @@ import { useSelectedLayoutSegments } from 'next/navigation';
 import {
   Breadcrumb,
   BreadcrumbItem,
-  BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
-  BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
 
 export default function AppBreadcrumb() {
@@ -16,14 +14,6 @@ export default function AppBreadcrumb() {
   return (
     <Breadcrumb>
       <BreadcrumbList>
-        <BreadcrumbItem className='hidden md:block'>
-          <BreadcrumbLink href='#'> </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator className='hidden md:block' />
-        <BreadcrumbItem>
-          <BreadcrumbPage> </BreadcrumbPage>
-        </BreadcrumbItem>
-
         {segments.map((segment, index) => (
           <BreadcrumbItem>
             <BreadcrumbPage key={index}>{segment}</BreadcrumbPage>
