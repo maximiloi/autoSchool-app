a6b680f4abb4baa2a9e6f1caa12c5b590701ab77
maximiloi <maximiloi@gmail.com>
2025-02-03 13:50:29 +0300

    add toast to layout page

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,11 +1,8 @@
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { Separator } from '@radix-ui/react-separator';
 import AppSidebar from '@/components/app-sidebar';
 import AppBreadcrumb from '@/components/app-breadcrumb';
+import { Toaster } from '@/components/ui/toaster';
 
 import { jetBrainsMono } from './ui/fonts';
 import './globals.css';
@@ -17,23 +14,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='ru'>
+    <html lang="ru">
       <body className={`${jetBrainsMono.className} antialiased`}>
         <SidebarProvider>
           <AppSidebar />
           <main>
             <SidebarInset>
-              <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
-                <div className='flex items-center gap-2 px-4'>
-                  <SidebarTrigger className='-ml-1' />
-                  <Separator orientation='vertical' className='mr-2 h-4' />
+              <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                <div className="flex items-center gap-2 px-4">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator orientation="vertical" className="mr-2 h-4" />
                   <AppBreadcrumb />
                 </div>
               </header>
-              <section className='py-4 px-4'>{children}</section>
+              <section className="px-4 py-4">{children}</section>
             </SidebarInset>
           </main>
         </SidebarProvider>
+        <Toaster />
       </body>
     </html>
   );
