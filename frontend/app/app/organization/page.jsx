260c9d396dc1324043472833e72203cf17f54e40
maximiloi <maximiloi@gmail.com>
2025-02-11 19:00:06 +0300

    feat: add tabs to page

--- frontend/app/app/organization/page.jsx
+++ frontend/app/app/organization/page.jsx
@@ -1,3 +1,4 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import OrganizationForm from './components/organization';
 import TeachersTable from './components/teachers-table';
@@ -5,24 +6,32 @@ import TeachersForm from './components/teachers';
 
 export default function OrganizationInfo() {
   return (
-    <div className="grid grid-cols-1 gap-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Реквизиты организации</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <OrganizationForm />
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Преподаватели организации</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <TeachersTable />
-          <TeachersForm />
-        </CardContent>
-      </Card>
-    </div>
+    <Tabs defaultValue="organization" className="w-full">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="organization">Реквизиты</TabsTrigger>
+        <TabsTrigger value="teachers">Преподаватели</TabsTrigger>
+      </TabsList>
+      <TabsContent value="organization">
+        <Card>
+          <CardHeader>
+            <CardTitle>Реквизиты организации</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <OrganizationForm />
+          </CardContent>
+        </Card>
+      </TabsContent>
+      <TabsContent value="teachers">
+        <Card>
+          <CardHeader>
+            <CardTitle>Преподаватели организации</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <TeachersTable />
+            <TeachersForm />
+          </CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
   );
 }
