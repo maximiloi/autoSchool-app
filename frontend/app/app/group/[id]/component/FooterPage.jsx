142debba42e25f2cdc352a55272040b1e596a419
maximiloi <maximiloi@gmail.com>
2025-02-17 13:40:54 +0300

    refactor: add new component

---
+++ frontend/app/app/group/[id]/component/FooterPage.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import { ScrollText } from 'lucide-react';
+
+export default function FooterPage() {
+  return (
+    <Button variant="secondary">
+      <ScrollText />
+      Маршрутный лист
+    </Button>
+  );
+}
--- frontend/app/app/group/[id]/page.jsx
+++ frontend/app/app/group/[id]/page.jsx
@@ -6,16 +6,8 @@ import { format } from 'date-fns';
 import { ru } from 'date-fns/locale';
 import { Separator } from '@/components/ui/separator';
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
-import { CarFront, NotepadText, Pencil } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import TablePage from './component/TablePage';
+import FooterPage from './component/FooterPage';
 
 export default function GroupPage({ params }) {
   const { id } = useParams(params);
@@ -66,45 +58,9 @@ export default function GroupPage({ params }) {
         </p>
       </div>
       <Separator className="my-4" />
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[73px]"></TableHead>
-            <TableHead className="w-[20px]">#</TableHead>
-            <TableHead className="w-[200px]">ФИО</TableHead>
-            <TableHead className="w-[145px]">Дата рождения</TableHead>
-            <TableHead className="w-[80px]">Договор</TableHead>
-            <TableHead>Вождение</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {group.students.map((student, index) => (
-            <TableRow key={student.id}>
-              <TableCell>
-                <Button variant="ghost" size="icon">
-                  <Pencil />
-                </Button>
-              </TableCell>
-              <TableCell>{index + 1}</TableCell>
-              <TableCell className="font-medium">
-                {student.lastName} {student.firstName}{' '}
-                {student.middleName ? `${student.middleName.charAt(0)}.` : ''}
-              </TableCell>
-              <TableCell>{format(student.birthDate, 'dd/MM/yyyy', { locale: ru })}</TableCell>
-              <TableCell>
-                <Button variant="outline" size="icon">
-                  <NotepadText />
-                </Button>
-              </TableCell>
-              <TableCell>
-                <Button variant="outline" size="icon">
-                  <CarFront />
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <TablePage group={group} />
+      <Separator className="my-4" />
+      <FooterPage />
     </>
   );
 }
